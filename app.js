4231e4292fd500c4913d4d1ae8b47e0d0d27682b
Serhii Tserkovnyi <dizz413@gmail.com>
2021-09-11 15:21:36 +0300

    game

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board');
+const colors = ['#32a852', '#26718c', '#71268c', '#8c3726', '#e3710e', '#e7eb05', '#5deb05'];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', setColor);
+    board.append(square);
+    square.addEventListener('mouseleave', removeColor);
+
+}
+
+function setColor(event) {
+    let element = event.target;
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(event) {
+    let element = event.target;
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+
+}
